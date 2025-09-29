644101025991748675491f545a3e298336d22039
Pavel Goloveiko <iwtf@yandex.ru>
2025-09-29 23:06:49 +0300

    Перед неймингом 1

--- src/services/slices/_constructorSlice.ts
+++ src/services/slices/_constructorSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TConstructorIngredient } from '@utils-types';
+import { v4 as uuidv4 } from 'uuid';
 
 interface ConstructorState {
   bun: TConstructorIngredient | null;
@@ -15,24 +16,31 @@ const _constructorSlice = createSlice({
   name: 'constructor',
   initialState,
   reducers: {
-    setBun(state, action) {
+    setBun(state, action: PayloadAction<TConstructorIngredient | null>) {
+      console.log(action.payload);
+      console.log('state');
+      console.log(state.items);
       state.bun = action.payload;
     },
     addConstructorIngredient(
       state,
       action: PayloadAction<TConstructorIngredient>
     ) {
-      state.items.push(action.payload);
+      state.items.push({ ...action.payload, id: uuidv4() });
+      //state.items.push(action.payload);
+    },
+    removeConstructorIngredient(state, action: PayloadAction<string>) {
+      state.items = state.items.filter((i) => i.id !== action.payload);
+    },
+    moveConstructorIngredient(
+      state,
+      action: PayloadAction<{ fromIndex: number; toIndex: number }>
+    ) {
+      const { fromIndex, toIndex } = action.payload;
+      const [movedItem] = state.items.splice(fromIndex, 1);
+      state.items.splice(toIndex, 0, movedItem);
     },
     clearConstructor(state) {
-      state.bun = null;
-      state.items = [];
-    },
-    moveConstructorIngredient(state) {
-      state.bun = null;
-      state.items = [];
-    },
-    removeConstructorIngredient(state) {
       state.bun = null;
       state.items = [];
     }
